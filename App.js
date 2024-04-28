3150b46a1a4e80e5248194a682ff44acee1c85c2
thxghntr <trinhdamhuy@gmail.com>
2024-04-28 23:02:44 +0700

    Add screen recording functionality and dependencies

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Button, PermissionsAndroid} from 'react-native';
 import CountingService from './src/modules/CountingServiceModule';
+
+import RecordScreen, {RecordingResult} from 'react-native-record-screen';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -9,7 +11,7 @@ const App = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       if (isCounting) {
-        setCount((prevCount) => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
       }
     }, 1000);
 
@@ -27,13 +29,60 @@ const App = () => {
   };
   const allow = () => {
     CountingService.allowPermission();
-  }
+  };
+
+  const [isRecording, setIsRecording] = useState(false);
+
+  const startRecording = async () => {
+    try {
+      const granted = await PermissionsAndroid.requestMultiple(
+        [PermissionsAndroid.PERMISSIONS.RECORD_AUDIO],
+        {
+          title: 'Screen Recording Permission',
+          message: 'Do you want to record with audio?',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+
+      if (
+        granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] ===
+        PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        setIsRecording(true);
+        RecordScreen.startRecording().catch(error => console.error(error));
+      } else {
+        setIsRecording(true);
+        RecordScreen.startRecording({mic: false}).catch(error =>
+          console.error(error),
+        );
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const stopRecording = async () => {
+    // recording stop
+    const res = await RecordScreen.stopRecording().catch(error =>
+      console.warn(error),
+    );
+    if (res) {
+      const url = res.result.outputURL;
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>Count: {count}</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 24, marginBottom: 20}}>Count: {count}</Text>
       <Button title="Start" onPress={startCounting} disabled={isCounting} />
       <Button title="Stop" onPress={stopCounting} disabled={!isCounting} />
-      <Button title="Allow permission" onPress={allow}/>
+      <Button title="Allow permission" onPress={allow} />
+      <Button
+        title={isRecording ? 'Stop Recording' : 'Start Recording'}
+        onPress={isRecording ? stopRecording : startRecording}
+      />
     </View>
   );
 };
