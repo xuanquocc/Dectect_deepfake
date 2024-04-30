1da4fdb0176f363b1bcb4664a1518a068ac2dc2b
thxghntr <trinhdamhuy@gmail.com>
2024-04-30 20:10:20 +0700

    Add okhttp3 library to the Android project dependencies

--- App.js
+++ App.js
@@ -4,16 +4,7 @@ import {View, Text, Button, NativeModules} from 'react-native';
 const {ScreenRecordModule, CountingServiceModule} = NativeModules;
 
 const App = () => {
-  const [isRecording, setIsRecording] = useState(false);
-  const startRecording = () => {
-    setIsRecording(true);
-    ScreenRecordModule.startScreenRecording();
-  };
-
-  const stopRecording = () => {
-    setIsRecording(false);
-    ScreenRecordModule.stopScreenRecording();
-  };
+  let buttonDisabled = false;
   const start = () => {
     CountingServiceModule.startMusic();
   };
@@ -24,15 +15,17 @@ const App = () => {
   const allow = () => {
     CountingServiceModule.allowPermission();
   };
+
+  const startRecording = () => {
+    ScreenRecordModule.startScreenRecording();
+  };
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Button title="Start music" onPress={start} />
       <Button title="Stop music" onPress={stop} />
       <Button title="Allow permission" onPress={allow} />
-      <Button
-        title={isRecording ? 'Stop Recording' : 'Start Recording'}
-        onPress={isRecording ? stopRecording : startRecording}
-      />
+      <Button title="Start Recording" onPress={startRecording} />
     </View>
   );
 };
--- android/app/src/main/java/com/deepfakeapp/ScreenRecordModule.java
+++ android/app/src/main/java/com/deepfakeapp/ScreenRecordModule.java
@@ -12,6 +12,7 @@ import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
@@ -27,17 +28,28 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ScreenRecordModule extends ReactContextBaseJavaModule implements ActivityEventListener {
     private static final String MODULE_NAME = "ScreenRecordModule";
     private static final int REQUEST_CODE_SCREEN_RECORD = 1;
+    private static final String API_URL = "https://api-deepfake-detection-1.onrender.com/upload-video/";
     private final MediaProjectionManager mediaProjectionManager;
     private MediaProjection mediaProjection;
     private VirtualDisplay virtualDisplay;
@@ -75,6 +87,50 @@ public class ScreenRecordModule extends ReactContextBaseJavaModule implements Ac
             mediaProjection.stop();
             mediaProjection = null;
         }
+
+        sendVideoToApi();
+    }
+
+    private void sendVideoToApi(){
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS) // Increase the connection timeout
+                .readTimeout(30, TimeUnit.SECONDS) // Increase the read timeout
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        File videoFile = new File(setupFilePath());
+
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("video_file", videoFile.getName(),
+                        RequestBody.create(videoFile, MediaType.parse("video/mp4")))
+                .build();
+
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+                // Read the response
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    Log.d(TAG, "Result: " + jsonObject.getBoolean("result"));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.e(TAG, "onFailure: " + e);
+            }
+        });
     }
 
     // Quay màn hình
@@ -133,6 +189,8 @@ public class ScreenRecordModule extends ReactContextBaseJavaModule implements Ac
     private void startScreenCapture() {
         Executor executor = Executors.newSingleThreadExecutor();
         executor.execute(() -> mediaRecorder.start());
+
+        new Handler().postDelayed(this::stopScreenRecording, 3000);
     }
 
     // Lớp con để xử lý sự kiện khi MediaProjection dừng
@@ -169,14 +227,9 @@ public class ScreenRecordModule extends ReactContextBaseJavaModule implements Ac
     private String setupFilePath() {
         // Lấy thư mục Videos trong bộ nhớ ngoài
         File directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES), "DeepfakeApp");
-        // Tạo định dạng ngày tháng cho tên file
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
-        // Lấy ngày và giờ hiện tại
-        String currentTimeStamp = dateFormat.format(new Date());
-        // Tạo tên file dựa trên ngày và giờ hiện tại
-        String fileName = "record_" + currentTimeStamp + ".mp4";
+
         // Tạo đường dẫn đầy đủ của file
-        String filePath = directory.getAbsolutePath() + "/" + fileName;
+        String filePath = directory.getAbsolutePath() + "/deepfake_check.mp4";
 
         // Tạo thư mục nếu nó không tồn tại
         if (!directory.exists()) {
