3aa6ecc232091bcd3a8989e7d8c1d6eee6e1b19c
thxghntr <trinhdamhuy@gmail.com>
2024-05-04 12:58:14 +0700

    chore: Add NotificationToOpenAppService for opening the app from a notification

---
+++ android/app/src/main/java/com/deepfakeapp/services/NotificationToOpenAppService.java
@@ -0,0 +1,59 @@
+package com.deepfakeapp.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.deepfakeapp.R;
+import com.deepfakeapp.ScreenRecordActivity;
+
+public class NotificationToOpenAppService extends Service {
+    private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "NotificationToOpenAppServiceChannel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Notification to open app",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    // Create a notification channel for Android Oreo and above
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        Intent activityIntent = new Intent(getApplicationContext(), ScreenRecordActivity.class);
+        activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, activityIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        // Create a notification for the foreground service
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Notification notification = new Notification.Builder(this, CHANNEL_ID)
+                    .setContentTitle("ninja gi gi do")
+                    .setContentText("We can only detect deepfake video call if you click this notification and allow us!")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(pendingIntent)
+                    .setOngoing(true)
+                    .build();
+            startForeground(NOTIFICATION_ID, notification);
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
