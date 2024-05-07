df30763f86339bc5625dcc2d99f5beb34a8109da
thxghntr <trinhdamhuy@gmail.com>
2024-05-07 10:47:10 +0700

    chore: Update screen recording code to use detected notification service

--- android/app/src/main/java/com/deepfakeapp/services/CallDetectionService.java
+++ android/app/src/main/java/com/deepfakeapp/services/CallDetectionService.java
@@ -1,8 +1,9 @@
-package com.deepfakeapp;
+package com.deepfakeapp.services;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.os.Build;
+import android.os.Parcelable;
 import android.view.accessibility.AccessibilityEvent;
 
 import androidx.annotation.RequiresApi;
@@ -17,14 +18,19 @@ public class CallDetectionService extends AccessibilityService {
         int eventType = event.getEventType();
         if (eventType == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
             // Kiểm tra thông báo
-            String eventText = event.getPackageName().toString();
-            if ((("com.facebook.orca".equals(eventText)
-                    && event.getParcelableData().toString().contains("voip_incoming"))
-                    || ("com.zing.zalo".equals(eventText)
-                    && event.getParcelableData().toString().contains("call_channel")))) {
-                NotificationToOpenApp notification = new NotificationToOpenApp(this);
-                notification.showNotification();
+            Parcelable parcelable = event.getParcelableData();
+
+            if (parcelable != null) {
+                String data = parcelable.toString();
+                if ((data.contains("messenger_orca")
+                        && data.contains("voip_incoming"))
+                        || (data.contains("zalo")
+                        && data.contains("call_channel"))) {
+                    NotificationToOpenApp notification = new NotificationToOpenApp(this);
+                    notification.showNotification();
+                }
             }
+
         }
     }
 
